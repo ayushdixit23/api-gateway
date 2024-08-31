31f6a2ea3d298a52738e3705924648a8fd65ac40
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-08-31 17:33:19 +0530

    43

--- routes/index.js
+++ routes/index.js
@@ -20,68 +20,6 @@ const upload = multer({
 
 const FormData = require('form-data');
 
-// router.all("/:apiName/*", upload.any(), async (req, res) => {
-// 	try {
-// 		const { apiName } = req.params;
-// 		const path = req.params[0];
-
-// 		if (registry.services[apiName]) {
-
-// 			const formattedPath = path.startsWith('/') ? path.substring(1) : path;
-// 			const baseUrl = `${registry.services[apiName].url}${registry.services[apiName].apiName}/${formattedPath}`;
-
-// 			const queryString = new URLSearchParams(req.query).toString();
-// 			const fullUrl = queryString ? `${baseUrl}?${queryString}` : baseUrl;
-
-// 			console.log('Request method:', req.method);
-// 			console.log('Request headers:', req.headers);
-// 			console.log('Request body:', req.body);
-// 			console.log('Request file:', req.files);
-
-// 			let axiosConfig = {
-// 				method: req.method,
-// 				url: fullUrl,
-// 				headers: {}
-// 			};
-
-// 			if (req.files && req.files.length > 0) {
-// 				const formData = new FormData();
-
-// 				// Append files
-// 				req.files.forEach(file => {
-// 					formData.append(file.fieldname, file.buffer, { filename: file.originalname, contentType: file.mimetype });
-// 				});
-
-// 				// Append other fields
-// 				Object.keys(req.body).forEach(key => {
-// 					formData.append(key, req.body[key]);
-// 				});
-
-// 				axiosConfig.data = formData;
-// 				axiosConfig.headers = {
-// 					"Content-Type": "multipart/form-data"
-// 				};
-// 			} else {
-// 				axiosConfig.data = req.body;
-// 				axiosConfig.headers['Content-Type'] = 'application/json';
-// 			}
-
-// 			// console.log(axiosConfig, "axios")
-// 			const resp = await axios(axiosConfig);
-
-// 			// console.log("Response received from service:", resp.data);
-// 			res.send(resp.data);
-// 		} else {
-// 			console.log("API Name doesn't exist");
-// 			res.status(404).send("API Name doesn't exist!");
-// 		}
-// 	} catch (error) {
-// 		console.error("Error handling request:", error);
-// 		res.status(400).send("Internal Server Error");
-// 	}
-// });
-
-
 router.all('/:apiName/*', upload.any(), async (req, res) => {
 	try {
 		const { apiName } = req.params;
@@ -105,11 +43,11 @@ router.all('/:apiName/*', upload.any(), async (req, res) => {
 				headers: {}
 			};
 
-			if (req.files && req.files.length > 0) {
+			if (req?.files && req?.files?.length > 0) {
 				const formData = new FormData();
 
 				// Append files by reading from disk (streaming)
-				req.files.forEach(file => {
+				req.files?.forEach(file => {
 					formData.append(file.fieldname, fs.createReadStream(file.path), { filename: file.originalname, contentType: file.mimetype });
 				});
 
@@ -132,7 +70,9 @@ router.all('/:apiName/*', upload.any(), async (req, res) => {
 			res.send(resp.data);
 
 			// Clean up the temporary files
-			req.files.forEach(file => fs.unlinkSync(file.path));
+			if (req?.files) {
+				req.files?.forEach(file => fs.unlinkSync(file.path));
+			}
 		} else {
 			console.log("API Name doesn't exist");
 			res.status(404).send("API Name doesn't exist!");
